7282fddcfa53b367efad8284ec9f0bde2d3be0a6
erhelito <72103557+erhelito@users.noreply.github.com>
2021-04-05 12:02:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import os
 import tkinter
 
 def ouvrir_app():
-    os.system("the_path_to_the_.sh_file")
+    os.system("the_path_to_launching.sh_file")
 
 fenetre = tkinter.Tk()
 label = tkinter.Label(fenetre, text = "Launch applications ?")
 label.pack()
 
-bouton1 = tkinter.Button(fenetre, text = "oui", command = ouvrir_app)
+bouton1 = tkinter.Button(fenetre, text = "yes", command = ouvrir_app)
 bouton1.pack()
 
-bouton2 = tkinter.Button(fenetre, text = "non", command = fenetre.quit)
+bouton2 = tkinter.Button(fenetre, text = "no", command = fenetre.quit)
 bouton2.pack()
 
 fenetre.mainloop()
